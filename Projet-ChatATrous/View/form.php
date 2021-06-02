2de94079e3a679e6cb8cf0df3971718ed16c68b9
Elsa Rhudye Mangoye <diamant.rhudye@gmail.com>
2021-06-02 14:01:29 +0200

    forView

--- Projet-ChatATrous/View/form.php
+++ Projet-ChatATrous/View/form.php
@@ -1,14 +1,27 @@
 <!-- Créer ici un formulaire avec les champs pseudo et message -->
 
+<form>
+    <fieldset>
+        <div class="row">
+            <div class="form-group col-4">
+                <label for="pseudo_fe" class="form-label mt-4" hidden>Pseudo</label>
+                <input type="text" class="form-control" id="pseudo_fe" aria-describedby="pseudo" placeholder="Entrez votre pseudo">
+            </div>
+
+            <div class="form-group col-8">
+                <label for="message_fe" class="form-label mt-4" hidden>message</label>
+                <textarea class="form-control" id="message_fe" rows="3" placeholder="Entrez votre message"></textarea>
+            </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Envoyer</button>
+
+    </fieldset>
+</form>
+
 <!--<p>
     Cette page ne contient que du HTML.<br />
     Veuillez taper votre prénom :
 </p> -->
 
-<form action="cible.php" method="post">
-<p>
-    <input type="text" name="prenom" />
-    <input type="submit" value="Valider" />
-    <input type="date" value=""
-</p>
-</form>
+
