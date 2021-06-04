c411e0b5b62541ca827ba499f86509de86beae32
Elsa Rhudye Mangoye <diamant.rhudye@gmail.com>
2021-06-04 13:39:23 +0200

    fix chat

--- view/chat.php
+++ view/chat.php
@@ -10,11 +10,11 @@
     <!-- Dans une premier temps faites une boucle pour afficher "en dur" plusieurs messages (plusieurs fois le même) -->
     <?php
         if (isset($messages)){
-            foreach($messages as $row){ ?>
+            foreach($messages as $message){ ?>
             <tr class="table-light">
-                <td class="col-2"><?= $row['date'] ?></td>
-                <td class="col-2"><?= $row['pseudo'] ?></td>
-                <td class="col-8"><?=nl2br($row['content'])?></td>
+                <td class="col-2"><?= $message['date'] ?></td>
+                <td class="col-2"><?= $message['pseudo'] ?></td>
+                <td class="col-8"><?=nl2br($message['content'])?></td>
             </tr>               
     <?php
          }}
